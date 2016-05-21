6d3bb8b2d49554c92e5097681401e1f8cb892796
Webvolution <spham@webvolutionchicago.com>
2016-05-21 15:37:59 -0500

    skeleton integration & home

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <main role="main">
-<?php if (have_posts()) :
+<?php
+if (have_posts()) {
   while (have_posts()) : the_post(); ?>
     <article>
       <?php the_content(); ?>
@@ -12,15 +13,31 @@
     </aside>
     -->
 
-  <?php endwhile;
+    <?php
+  endwhile;
 
-  elseif (is_404()) : ?>
+  if (is_front_page()):
+    ?>
+    <aside>
+      <div class="row">
+        <div class="three columns col1">11</div>
+        <div class="three columns col2">22</div>
+        <div class="three columns col3">33</div>
+        <div class="three columns col4">44</div>
+      </div>
+    </aside>
+
+    <?php
+  endif;
+
+}elseif (is_404()){ ?>
 
   <article>
     <h1>We're sorry...</h1>
     <p>Looks like we can't find the page you are looking for!</p>
   </article>
 
-  <?php endif; ?>
+<?php } ?>
+
 </main>
 <?php get_footer(); ?>
